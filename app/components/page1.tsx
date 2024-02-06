ec81c03e3535184718a7a657224b1e1a7284a246
Baterdene Ochirbal <baterdene@ema.gov.mn>
2024-02-06 17:50:35 +0800

    mobile ui fix

--- app/components/page1.tsx
+++ app/components/page1.tsx
@@ -1,24 +1,32 @@
 import Image from "next/image";
 
 export default function Page1() {
-    return <div className="page">
-        <p className="largetypography helvetica bold">EXPERIMENTS</p>
-        <div className="hero">
-            <div className="container">
-
-
-                <p>ᠥᠰᢈᠦ ᠡᠴᠡ ᠰᠤᠷᠤᠭᠰᠠᠨ ᠦᠨᠳᠦᠰᠦᠨ ᠦ᠋ ᢈᠡᠯᠡ ᠮᠠᠷᠲᠠᠵᠤ ᠪᠣᠯᠤᠰᠢ ᠦ᠋ᢉᠡᠢ ᠰᠣᠶᠤᠯ
-                    ᠦᢈᠦᠲᠡᠯ᠎ᠡ ᠣᠷᠤᠰᠢᠬᠤ ᠲᠥᠷᠦᠯᢈᠢ ᠨᠤᠲᠤᠭ ᠰᠠᠯᠵᠤ ᠪᠣᠯᠤᠰᠢ ᠦ᠋ᢉᠡᠢ ᠣᠷᠤᠨ᠃</p>
-                <br />
-                <div className="profile">
-                    <Image src={"/avatar.png"} width={60} height={60} alt={""} priority={true} />
-                    <p> ᠳᠤ᠃ ᠨᠠᠴᠣᠭᠳᠣᠷᠵᠢ</p>
-                </div>
-            </div>
+  return (
+    <div className="page">
+      <p className="largetypography helvetica bold">EXPERIMENTS</p>
+      <div className="hero">
+        <div className="container">
+          <p>
+            ᠥᠰᢈᠦ ᠡᠴᠡ ᠰᠤᠷᠤᠭᠰᠠᠨ ᠦᠨᠳᠦᠰᠦᠨ ᠦ᠋ ᢈᠡᠯᠡ ᠮᠠᠷᠲᠠᠵᠤ ᠪᠣᠯᠤᠰᠢ ᠦ᠋ᢉᠡᠢ ᠰᠣᠶᠤᠯ
+            ᠦᢈᠦᠲᠡᠯ᠎ᠡ ᠣᠷᠤᠰᠢᠬᠤ ᠲᠥᠷᠦᠯᢈᠢ ᠨᠤᠲᠤᠭ ᠰᠠᠯᠵᠤ ᠪᠣᠯᠤᠰᠢ ᠦ᠋ᢉᠡᠢ ᠣᠷᠤᠨ᠃
+          </p>
+          <br />
+          <div className="profile">
+            <Image
+              src={"/avatar.png"}
+              width={60}
+              height={60}
+              alt={""}
+              priority={true}
+            />
+            <p> ᠳᠤ᠃ ᠨᠠᠴᠣᠭᠳᠣᠷᠵᠢ</p>
+          </div>
         </div>
-        <div className="text">
-            <p>ᠮᠣᠩᠭᠤᠯ ᠬᠡᠯᠡ</p>
-            <p>ᠪᠢᠴᠢᠭ ᠰᠣᠶᠤᠯ</p>
-        </div>
+      </div>
+      <div className="text">
+        <p>ᠮᠣᠩᠭᠤᠯ ᠬᠡᠯᠡ</p>
+        <p>ᠪᠢᠴᠢᠭ ᠰᠣᠶᠤᠯ</p>
+      </div>
     </div>
+  );
 }
